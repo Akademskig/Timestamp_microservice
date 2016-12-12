4a5018db9e7cf82d38eaa62997dc3b306a8b0391
Marta Sušek <m.susek@live.com>
2016-12-12 11:57:31 +0000

    fixed errors

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var app = express();
 var path = require('path')
 var fs = require('fs')
 var moment = require('moment');
-
 var styles = path.join(__dirname, '.');
 
 app.use(require('stylus').middleware(styles))
@@ -17,22 +16,21 @@ app.get('/', function(req, res){
 
 app.get('/:timestring', function(req, res){
     var date;
-    try{
-        if(req.params.timestring.parseInt()){
-            date = req.params.timestring;
-        }
+    if(req.url=='/favicon.ico'){
+        return;
     }
-    catch(err){
+    if(parseInt(req.params.timestring)){
+        date = moment(req.params.timestring, "X");
+    }
+    else{
         date = moment(req.params.timestring, "MMMM, D, YYYY");
     }
     
-    var d = new Date(date);
-    console.log(d);
-    if(d != "Invalid Date"){
+    
+    if(date){
         res.send({
-            unix: d.now(),
-            natural: d.toDateString()
-        })
+            unix: date.format("X"),
+            natural: date.format("MMMM D, YYYY")})
     }
     else{
        res.send({
@@ -40,6 +38,7 @@ app.get('/:timestring', function(req, res){
             natural: null
         })
     }
+    
 })
 
 app.listen(process.env.PORT || 8080, function(){
